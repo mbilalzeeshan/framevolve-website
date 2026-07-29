a94ee26ed435595ca5107e1c8d17d22e11062d9e
Muhammad Bilal Zeeshan <mbzgamer500@gmail.com>
2026-07-29 18:09:09 +0500

    Create project foundation

---
+++ lib/constants.ts
@@ -0,0 +1,33 @@
+export const COMPANY = {
+  name: "Framevolve",
+  email: "hello@framevolve.com",
+  phone: "+1 (000) 000-0000",
+
+  socials: {
+    linkedin: "https://linkedin.com/company/framevolve",
+    instagram: "https://instagram.com/framevolve",
+  },
+};
+
+export const PRICING = [
+  {
+    title: "Single Video",
+    videos: 1,
+    price: 80,
+  },
+  {
+    title: "Professional",
+    videos: 10,
+    price: 720,
+  },
+  {
+    title: "Agency",
+    videos: 25,
+    price: 1720,
+  },
+  {
+    title: "Enterprise",
+    videos: 50,
+    price: 3360,
+  },
+];
